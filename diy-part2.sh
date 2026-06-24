73bc432094325b98ba3f2a798a1434218a647cad
WASNHDF <52650540@qq.com>
2026-06-24 17:34:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-# 1. 移除不必要的模块，保持固件轻量
-sed -i '/luci-app-diag-core/d' .config
-sed -i '/luci-app-statistics/d' .config
-sed -i '/luci-app-accesscontrol/d' .config
-sed -i '/luci-app-nlbwmon/d' .config
-
-# 2. 仅保留 Argon 主题，移除其它臃肿的主题
-sed -i '/luci-theme-bootstrap/d' .config
-sed -i '/luci-theme-material/d' .config
-sed -i '/luci-theme-openwrt/d' .config
-
-# 3. 确保系统核心功能完好
-echo 'CONFIG_PACKAGE_dnsmasq-full=y' >> .config
-echo 'CONFIG_PACKAGE_firewall=y' >> .config
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
