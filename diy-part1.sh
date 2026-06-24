28fea7d9415b850db3d3f3e8f034be9d99bf9949
WASNHDF <52650540@qq.com>
2026-06-24 17:16:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,3 +12,9 @@
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # 在 diy-part1.sh 文件中添加以下行：
+# 拉取Passwall核心插件
+git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+# 拉取luci前端面板
+git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
+# 可选：配套smartdns（原仓库也删了，需要就加上）
+# git clone https://github.com/pymumu/smartdns package/smartdns
